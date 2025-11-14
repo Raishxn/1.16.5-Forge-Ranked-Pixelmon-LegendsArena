e64e0e5becae78ebaa14f82678dba924bed1b8f1
raishxns <erickrenato2019@gmail.com>
2025-11-14 11:24:39 -0300

    0.2

--- src/main/java/com/raishxn/legendsarena/legendsarena.java
+++ src/main/java/com/raishxn/legendsarena/legendsarena.java
@@ -1,23 +1,19 @@
 package com.raishxn.legendsarena;
 
-import com.envyful.api.config.yaml.YamlConfigFactory;
 import com.envyful.api.forge.command.ForgeCommandFactory;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-
 @Mod("legendsarena")
-public class ModFile {
+public class legendsarena {
 
     public static final String MOD_ID = "legendsarena";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-    private static ModFile instance;
+    private static legendsarena instance;
 
     private final ForgeCommandFactory commandFactory = new ForgeCommandFactory();
 
@@ -30,7 +26,7 @@ public class ModFile {
 
     }
 
-    public static ModFile getInstance() {
+    public static legendsarena getInstance() {
         return instance;
     }
     }
